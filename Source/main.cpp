7cf6c814ef96a42ae9fd088fc15ab002a2994676
Joshua Crinall <sc14jrc@leeds.ac.uk>
2017-10-13 13:32:42 +0100

    Project - Add GLFW window and main loop

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* window;
 
 int main(int argc, const char* argv[])
 {
-	std::cout << "Game running" << std::endl;
+	// Initialise GLFW library
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	// Ensure OpenGL4.5 is supported - exit if not
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+	// Create a window and its OpenGL context - exit with failure if window not initialised
+	window = glfwCreateWindow(1280, 720, "hello gaem", NULL, NULL);
+	if(!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make window's context current
+	glfwMakeContextCurrent(window);
+
+    printf("Initializing OpenGL %s \n", glGetString(GL_VERSION));
+
+	// Run game loop while window not closed
+	while (!glfwWindowShouldClose(window))
+	{
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap front & back framebuffers
+        glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
