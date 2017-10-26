3d9a3a4fd492167312fecccfd9ad6fa5130486e3
George Loines <loinesg@gmail.com>
2017-10-26 16:52:06 +0100

    Replace BitReaderTests and BitWriterTests tabs with spaces

--- Tests/Utils/BitReaderTests.cpp
+++ Tests/Utils/BitReaderTests.cpp
@@ -9,48 +9,48 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UtilTests
 {
-	TEST_CLASS(BitReaderTests)
-	{
+    TEST_CLASS(BitReaderTests)
+    {
 
-	public:
+    public:
 
-		TEST_METHOD(Constructor)
-		{
-			uint32_t* buffer = new uint32_t[1];
-			BitReader r = BitReader(buffer);
-		}
+        TEST_METHOD(Constructor)
+        {
+            uint32_t* buffer = new uint32_t[1];
+            BitReader r = BitReader(buffer);
+        }
 
-		TEST_METHOD(CheckReadByte)
-		{
-			BitWriter b;
-			b.writeByte(4);
-			b.writeByte(3);
+        TEST_METHOD(CheckReadByte)
+        {
+            BitWriter b;
+            b.writeByte(4);
+            b.writeByte(3);
 
-			uint32_t* buffer = b.getBuffer();
-			BitReader r = BitReader(buffer);
-			int t1 = r.readByte();
-			int t2 = r.readByte();
-			Assert::AreEqual(4,t1);
-			Assert::AreEqual(3, t2);
+            uint32_t* buffer = b.getBuffer();
+            BitReader r = BitReader(buffer);
+            int t1 = r.readByte();
+            int t2 = r.readByte();
+            Assert::AreEqual(4, t1);
+            Assert::AreEqual(3, t2);
 
-		}
+        }
 
-		TEST_METHOD(CheckReadBits)
-		{
-			BitWriter b;
-			b.writeBits(4, 3);
-			b.writeBits(3, 10);
-			b.writeBits(5, 24);
+        TEST_METHOD(CheckReadBits)
+        {
+            BitWriter b;
+            b.writeBits(4, 3);
+            b.writeBits(3, 10);
+            b.writeBits(5, 24);
 
-			uint32_t* buffer = b.getBuffer();
-			BitReader r = BitReader(buffer);
-			int t1 = r.readBits(3);
-			int t2 = r.readBits(10);
-			int t3 = r.readBits(24);
-			Assert::AreEqual(4, t1);
-			Assert::AreEqual(3, t2);
-			Assert::AreEqual(5, t3);
-		}
+            uint32_t* buffer = b.getBuffer();
+            BitReader r = BitReader(buffer);
+            int t1 = r.readBits(3);
+            int t2 = r.readBits(10);
+            int t3 = r.readBits(24);
+            Assert::AreEqual(4, t1);
+            Assert::AreEqual(3, t2);
+            Assert::AreEqual(5, t3);
+        }
 
-	};
+    };
 }
