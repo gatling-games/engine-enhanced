769e42f6e11a32d7cce50be2858dae50031b7c5d
George Loines <loinesg@gmail.com>
2017-10-31 11:46:40 +0000

    Add resource output file hashing

--- Source/ResourceManager.h
+++ Source/ResourceManager.h
@@ -9,18 +9,22 @@ template <class SettingsT>
 class Resource
 {
 public:
-	virtual bool Load(const SettingsT* settings, std::ifstream &file) = 0;
-	virtual void Unload() = 0;
+    virtual bool Load(const SettingsT* settings, std::ifstream &file) = 0;
+    virtual void Unload() = 0;
 };
 
 class ResourceImporter
 {
 public:
-	virtual bool canHandleFileType(const std::string &fileExtension) const = 0;
-	virtual bool importFile(const std::string &sourceFile, const std::string &outputFile) const = 0;
+    virtual bool canHandleFileType(const std::string &fileExtension) const = 0;
+    virtual bool importFile(const std::string &sourceFile, const std::string &outputFile) const = 0;
 };
 
 class ResourceManager
 {
+public:
 
+    // Determines the resource id for a given source path.
+    // The path MUST be relative to the Resources folder (eg Textures/wood_diffuse.png)
+    static ResourceID pathToResourceID(const std::string &path);
 };
