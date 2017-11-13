056b1f9f61730902523f8939a5120f156028f101
Michael Nisbet <sc14m2n@leeds.ac.uk>
2017-11-13 15:51:45 +0000

    Added filetype checking

--- Source/Importers/ShaderImporter.cpp
+++ Source/Importers/ShaderImporter.cpp
@@ -1,2 +1,20 @@
 #include "ShaderImporter.h"
 
+bool ShaderImporter::canHandleFileType(const std::string& fileExtension) const
+{
+    // Support the following file extensions.
+    const int numTypes = 2;
+    const std::string supportedTypes[numTypes] =  {".shader", ".shaderinclude"};
+
+    // Check if the extension is supported.
+    for (int i = 0; i < numTypes; ++i)
+    {
+        if (supportedTypes[i] == fileExtension)
+        {
+            return true;
+        }
+    }
+
+    // Not supported.
+    return false;
+}
