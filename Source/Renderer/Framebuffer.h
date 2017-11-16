8b4b01c2c01dbf0bc0d48d97b5281207e24bd97d
George Loines <loinesg@gmail.com>
2017-11-16 12:00:33 +0000

    Renderer - Add Framebuffer class

---
+++ Source/Renderer/Framebuffer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <GL/gl3w.h>
+
+class Texture;
+
+class Framebuffer
+{
+public:
+    Framebuffer();
+    ~Framebuffer();
+
+private:
+    // Constructor for internal use.
+    Framebuffer(GLuint id);
+
+public:
+    // Gets a reference to the main backbuffer (aka framebuffer 0).
+    static const Framebuffer* backbuffer();
+
+    // Prevent the framebuffer from being copied
+    Framebuffer(const Framebuffer&) = delete;
+    Framebuffer& operator=(const Framebuffer) = delete;
+
+    // Sets the depth texture used by the framebuffer.
+    void attachDepthTexture(const Texture* depthTexture);
+
+    // Sets the color texture used by the framebuffer.
+    void attachColorTexture(const Texture* colorTexture);
+
+    // Sets this framebuffer as the active framebuffer.
+    void use();
+
+private:
+    GLuint id_;
+};
