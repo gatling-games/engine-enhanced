4a29b32f62ba408bd33ad23834d2b03d4bef2b22
George Loines <loinesg@gmail.com>
2017-11-20 12:42:48 +0000

    Shaders - [Fix] Prevent error when reimporting a shader
    Delete any already-imported version of a shader resource to prevent
    filesystem errors during import.

--- Source/Importers/ShaderImporter.cpp
+++ Source/Importers/ShaderImporter.cpp
@@ -7,7 +7,7 @@ bool ShaderImporter::canHandleFileType(const std::string& fileExtension) const
 {
     // Support the following file extensions.
     const int numTypes = 2;
-    const std::string supportedTypes[numTypes] =  {".shader", ".shaderinclude"};
+    const std::string supportedTypes[numTypes] = { ".shader", ".shaderinclude" };
 
     // Check if the extension is supported.
     for (int i = 0; i < numTypes; ++i)
@@ -24,6 +24,16 @@ bool ShaderImporter::canHandleFileType(const std::string& fileExtension) const
 
 bool ShaderImporter::importFile(const std::string &sourceFile, const std::string &outputFile) const
 {
+    // If there is already an output file, delete it.
+    if (fs::exists(outputFile))
+    {
+        fs::remove(outputFile);
+    }
+
+    // Now copy the shader source directly to the output directory.
+    // The source code is passed to opengl at run time.
     fs::copy(sourceFile, outputFile);
+
+    // This importer always succeeds.
     return true;
 }
