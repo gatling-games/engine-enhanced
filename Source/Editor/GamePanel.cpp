d94c0234807e8b85493dd634f9ccbf0d5b29031e
Joshua Crinall <sc14jrc@leeds.ac.uk>
2017-11-21 16:43:44 +0000

    Mesh importer - Now triangulating quad meshes before import

--- Source/Editor/GamePanel.cpp
+++ Source/Editor/GamePanel.cpp
@@ -2,11 +2,17 @@
 
 #include <imgui.h>
 
-GamePanel::GamePanel()
+#include "Renderer/UniformBuffer.h"
+
+GamePanel::GamePanel(ResourceManager* resourceManager)
     : frameBuffer_(nullptr)
     , depthBuffer_(nullptr)
     , colorBuffer_(nullptr)
+    , resourceManager_(resourceManager)
+    , objects_()
+    , camera_(Point3(0.0f, 0.0f, 0.0f), Quaternion::identity())
 {
+
 }
 
 GamePanel::~GamePanel()
@@ -22,10 +28,46 @@ GamePanel::~GamePanel()
 
 void GamePanel::draw()
 {
+    // On first draw, load the scene and camera
+    if(objects_.empty())
+    {
+        Shader* cubeShader = resourceManager_->load<Shader>("Shader/ForwardPass.shader");
+        Mesh* cubeMesh = resourceManager_->load<Mesh>("Meshes/cube.obj");
+       // Texture* cubeTexture = resourceManager_->load<Texture>("Textures/cube.png");
+        Material* cubeMaterial = new Material();
+        cubeMaterial->shader = cubeShader;
+        cubeMaterial->texture = nullptr;
+        SceneObject cubeSO;
+        cubeSO.mesh = cubeMesh;
+        cubeSO.material = cubeMaterial;
+        cubeSO.localToWorld = Matrix4x4::trs(Vector3(0.0f, 0.0f, 10.0f), Quaternion::identity(), Vector3::one());
+        objects_.push_back(cubeSO);
+    }
+
     frameBuffer_->use();
 
-    glClear(GL_COLOR_BUFFER_BIT);
+    // Clear the screen
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glClearColor(180.0f, 0.0f, 180.0f, 1.0f);
+
+    // Setup the camera uniform buffer
+    CameraUniformData cud;
+    cud.cameraPosition = Vector4(camera_.getPosition());
+    cud.worldToClip = camera_.getWorldToCameraMatrix(1.0);
+    UniformBuffer<CameraUniformData> ub(UniformBufferType::CameraBuffer);
+    ub.update(cud);
+    ub.use();
+
+    for(unsigned int i = 0; i < objects_.size(); ++i)
+    {
+        // Set the correct mesh, shader, texture
+        objects_[i].mesh->bind();
+        // objects_[i].material->texture->bind(0);
+        objects_[i].material->shader->bind();
+
+        // Draw the mesh
+        glDrawElements(GL_TRIANGLES, objects_[i].mesh->elementsCount(), GL_UNSIGNED_SHORT, (void*)0);
+    }
 
     Framebuffer::backbuffer()->use();
 
--- Source/Editor/GamePanel.h
+++ Source/Editor/GamePanel.h
@@ -3,11 +3,28 @@
 #include "Editor/EditorPanel.h"
 #include "Renderer/Texture.h"
 #include "Renderer/Framebuffer.h"
+#include "Renderer/Mesh.h"
+#include "Renderer/Shader.h"
+#include "Renderer/Camera.h"
+
+// Todo: Move into Scene.h and Material.h
+struct Material
+{
+    Shader* shader;
+    Texture* texture;
+};
+
+struct SceneObject
+{
+    Mesh* mesh;
+    Material* material;
+    Matrix4x4 localToWorld;
+};
 
 class GamePanel : public EditorPanel
 {
 public:
-    GamePanel();
+    GamePanel(ResourceManager* resourceManager);
     ~GamePanel();
 
     // EditorPanel overrides
@@ -19,4 +36,10 @@ private:
     Framebuffer* frameBuffer_;
     Texture* depthBuffer_;
     Texture* colorBuffer_;
+
+    ResourceManager* resourceManager_;
+
+    // Todo: Move into Scene.h
+    std::vector<SceneObject> objects_;
+    Camera camera_;
 };
