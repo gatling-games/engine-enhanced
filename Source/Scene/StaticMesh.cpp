651111066c6538c01c75d0104e3658ff478b8203
George Loines <loinesg@gmail.com>
2017-11-21 15:17:51 +0000

    Resources - [Fix] Require Resources/ in resource source paths, support either direction of slash

--- Source/Scene/StaticMesh.cpp
+++ Source/Scene/StaticMesh.cpp
@@ -3,6 +3,6 @@
 StaticMesh::StaticMesh(const GameObjectID gameObjectID)
     : GameObjectComponent(gameObjectID)
 {
-    mesh_ = ResourceManager::instance()->load<Mesh>("Meshes/cube.obj");
-    shader_ = ResourceManager::instance()->load<Shader>("Shaders/ForwardPass.shader");
+    mesh_ = ResourceManager::instance()->load<Mesh>("Resources/Meshes/cube.obj");
+    shader_ = ResourceManager::instance()->load<Shader>("Resources/Shaders/ForwardPass.shader");
 }
