2b24bee3c4f357b5ff311e1e8cc2ffea69d57da9
Michael Nisbet <sc14m2n@leeds.ac.uk>
2017-11-30 17:03:31 +0000

    Added ASP.NET files for master server.

---
+++ MasterServer/Controllers/GamesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public string Get(string name = "Human")
+        {
+            return "Hello, " + name;
+        }
+    }
+}
