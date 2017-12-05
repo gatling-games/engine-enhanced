b4b28d0f41620751a29d05899ecbd4e77135f6fd
George Loines <loinesg@gmail.com>
2017-12-05 23:42:35 +0000

    GameObject - Implement StaticMesh properties panel

---
+++ Source/Utils/ImGuiExtensions.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <imgui.h>
+
+#include "ResourceManager.h"
+
+namespace ImGui
+{
+    template<typename T>
+    void ResourceSelectModal(const char* modalName, T* &resource)
+    {
+        // Get the source path of the currently selected resource
+        const ResourceID resourceID = (resource == nullptr) ? 0 : resource->id();
+        const std::string resourcePath = (resource == nullptr) ? "none" : ResourceManager::instance()->resourceIDToPath(resourceID);
+
+        if (ImGui::BeginPopupModal(modalName))
+        {
+            // Draw a button for each source file in the resource manager
+            const std::vector<std::string>* sourceFiles = ResourceManager::instance()->allSourceFiles();
+            for (unsigned int i = 0; i < sourceFiles->size(); ++i)
+            {
+                const std::string sourceFilePath = sourceFiles->at(i);
+                if (ImGui::Selectable(sourceFilePath.c_str(), sourceFilePath == resourcePath))
+                {
+                    resource = ResourceManager::instance()->load<T>(sourceFilePath);
+                    ImGui::CloseCurrentPopup();
+                }
+            }
+
+            // Display a cancel button at the bottom
+            if (ImGui::Button("Cancel", ImVec2(120, 0)))
+            {
+                ImGui::CloseCurrentPopup();
+            }
+
+            ImGui::EndPopup();
+        }
+    }
+
+    template<typename T>
+    void ResourceSelect(const char* label, const char* modalTitle, T* &resource)
+    {
+        // Push an extra ID so that multiple ResourceSelect()s work.
+        ImGui::PushID(label);
+
+        // Get the resource ID and source path
+        const ResourceID resourceID = (resource == nullptr) ? 0 : resource->id();
+        const std::string resourcePath = (resource == nullptr) ? "none" : ResourceManager::instance()->resourceIDToPath(resourceID);
+
+        // Show the resource name in a read-only text box
+        ImGui::InputText("", (char*)resourcePath.c_str(), resourcePath.length(), ImGuiInputTextFlags_ReadOnly);
+
+        // Then draw a button that opens the file selection modal
+        ImGui::SameLine();
+        if (ImGui::Button("..."))
+        {
+            ImGui::OpenPopup(modalTitle);
+        }
+
+        // Then draw the control label
+        ImGui::SameLine();
+        ImGui::Text(label);
+
+        // Finally draw the file selection modal (when open).
+        ResourceSelectModal<T>(modalTitle, resource);
+
+        // We are done with the ID.
+        ImGui::PopID();
+    }
+}
