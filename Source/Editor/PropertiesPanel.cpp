c3ae25bf1e9757c3c1db56374eaee7e7c3fc459e
George Loines <loinesg@gmail.com>
2017-12-10 14:35:21 +0000

    Components - Add Component::name() method

--- Source/Editor/PropertiesPanel.cpp
+++ Source/Editor/PropertiesPanel.cpp
@@ -59,12 +59,8 @@ void PropertiesPanel::drawComponentsSection()
         // Place a space before the component
         ImGui::Spacing();
 
-        // Get the component name from typeid
-        // All of the names begin with "Class ", so skip the first 6 chars
-        const char* componentName = typeid(*components[i]).name() + 6;
-
         // Put the component controls inside a drop down
-        if (ImGui::CollapsingHeader(componentName, ImGuiTreeNodeFlags_DefaultOpen))
+        if (ImGui::CollapsingHeader(components[i]->name().c_str(), ImGuiTreeNodeFlags_DefaultOpen))
         {
             components[i]->drawProperties();
         }
--- Source/Scene/Component.cpp
+++ Source/Scene/Component.cpp
@@ -10,3 +10,13 @@ Component::~Component()
 {
 
 }
+
+const std::string Component::name() const
+{
+    // Get the component name from typeid
+    const char* componentName = typeid(*this).name();
+
+    // The name begins with "Class " on msvc
+    // Skip the first 6 chars (5 in Class plus the space)
+    return std::string(componentName + 6);
+}
