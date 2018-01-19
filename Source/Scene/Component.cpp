01adc1697d100432d8d402f4718b57cfe09f4caf
George Loines <loinesg@gmail.com>
2018-01-19 14:07:55 +0000

    Fix compiler warnings in Component.h

--- Source/Scene/Component.cpp
+++ Source/Scene/Component.cpp
@@ -1,5 +1,7 @@
 #include "Component.h"
 
+#include <imgui.h>
+
 Component::Component(GameObject* gameObject)
     : gameObject_(gameObject)
 {
@@ -19,4 +21,19 @@ const std::string Component::name() const
     // The name begins with "Class " on msvc
     // Skip the first 6 chars (5 in Class plus the space)
     return std::string(componentName + 6);
+}
+
+void Component::update(float)
+{
+	// Do nothing
+}
+
+void Component::drawProperties()
+{
+	ImGui::Text("%s", "This component has no properties.");
+}
+
+void Component::serialize(PropertyTable&)
+{
+	// Do nothing
 }
