d058f3afbbe685c99d79baf03146b97102bca55f
George Loines <loinesg@gmail.com>
2018-01-30 23:33:03 +0000

    Remove old textures, add pbr-calibrated materials

--- Source/Renderer/Renderer.cpp
+++ Source/Renderer/Renderer.cpp
@@ -252,10 +252,10 @@ void Renderer::executeDeferredGBufferPass() const
         terrain->texture()->bind(1);
         terrain->normalMap()->bind(4);
         //THIS IS A HACK REMOVE LATER
-        ResourceManager::instance()->load<Texture>("Resources/Textures/terrain_snow.psd")->bind(2);
-        ResourceManager::instance()->load<Texture>("Resources/Textures/terrain_rock.png")->bind(3);
-        ResourceManager::instance()->load<Texture>("Resources/Textures/terrain_snow_normals.png")->bind(5);
-        ResourceManager::instance()->load<Texture>("Resources/Textures/terrain_rock_normals.tga")->bind(6);
+        ResourceManager::instance()->load<Texture>("Resources/Textures/ground_snow_albedo.tga")->bind(2);
+        ResourceManager::instance()->load<Texture>("Resources/Textures/ground_rock_01_albedo.tga")->bind(3);
+        ResourceManager::instance()->load<Texture>("Resources/Textures/ground_snow_normals.tga")->bind(5);
+        ResourceManager::instance()->load<Texture>("Resources/Textures/ground_rock_01_normals.tga")->bind(6);
 
         updateTerrainUniformBuffer(terrain);
 
