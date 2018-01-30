135923aa31ad35835549d6c8ca6146933f9f57d5
Michael Nisbet <nisbet.mr@gmail.com>
2018-01-30 00:28:10 +0000

    Added controls for layer textures and constraints

--- Source/Scene/Terrain.cpp
+++ Source/Scene/Terrain.cpp
@@ -19,7 +19,7 @@ Terrain::Terrain(GameObject* gameObject)
 void Terrain::drawProperties()
 {
     ImGui::ResourceSelect<Texture>("Heightmap", "Select Heightmap", heightMap_);
-    ImGui::ResourceSelect<Texture>("Base Texture", "Select Texture", baseTexture_);
+    ImGui::ResourceSelect<Texture>("Base Texture", "Select Base Texture", baseTexture_);
     ImGui::ResourceSelect<Texture>("Normal Map", "Select Normal Map", normalMap_);
     ImGui::DragFloat2("Texture Repeat", &textureWrap_.x, 0.1f, 1.0f, 1024.0f);
     ImGui::DragFloat3("Size", &dimensions_.x, 1.0f, 1.0f, 4096.0f);
@@ -29,9 +29,16 @@ void Terrain::drawProperties()
         {
             layerCount_ += 1;
         }
-        for (int layer = 0; layer < layerCount_; ++layer)
+        for (int layer = 0; layer < layerCount_; layer++)
         {
-            ImGui::InputText("Layer", (char*)&terrainLayers_[layer].name, sizeof(terrainLayers_[layer].name));
+			ImGui::PushID(layer);
+            ImGui::InputText("", (char*)&terrainLayers_[layer].name, sizeof(terrainLayers_[layer].name));
+			ImGui::DragFloat2("Height Span", &terrainLayers_[layer].minMaxHeight.x, 0.1f, 0.0f, 100.0f);
+			ImGui::DragFloat2("Angle Span", &terrainLayers_[layer].minMaxAngle.x, 0.1f, 0.0f, 90.0f);
+			ImGui::ResourceSelect<Texture>("Texture", "Select Layer Texture",terrainLayers_[layer].texture);
+			ImGui::ResourceSelect<Texture>("Normal Map", "Select Layer Normal Map", terrainLayers_[layer].normalMap);
+			ImGui::Spacing();
+			ImGui::PopID();
         }
         ImGui::TreePop();
     }
