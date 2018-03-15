f3dae36b67806da5fe5e13cce0af6f9b17e4dae8
George Loines <loinesg@gmail.com>
2018-03-15 12:35:11 +0000

    Renderer - [Fix] Reduce shadow map bias to prevent peter panning

--- Source/Renderer/ShadowMap.h
+++ Source/Renderer/ShadowMap.h
@@ -27,7 +27,7 @@ class ShadowMap
 public:
     static const int RESOLUTION = 4096; // Cannot increase past 16384 on most hardware
     static const int CASCADE_COUNT = 4; // Increasing past 4 will require reworking the uniform buffer layout
-    const float DEPTH_BIAS_PER_CASCADE[CASCADE_COUNT] = { 0.00125f, 0.0018f, 0.0035f, 0.009f };
+    const float DEPTH_BIAS_PER_CASCADE[CASCADE_COUNT] = { 0.0004f, 0.0005f, 0.001f, 0.0015f };
     const float SHADOW_DRAW_DISTANCE = 300.0f;
 
 public:
