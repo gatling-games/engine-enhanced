2c825bd710f939c90a52bdf26de69de49155b4eb
Michael Nisbet <nisbet.mr@gmail.com>
2018-04-20 18:39:09 +0100

    Added OutputPanel class

--- Source/Editor/OutputPanel.cpp
+++ Source/Editor/OutputPanel.cpp
@@ -2,5 +2,57 @@
 
 void OutputPanel::draw()
 {
+    if (ImGui::Button("Clear")) clear();
+    ImGui::SameLine();
+    bool copy = ImGui::Button("Copy");
+    ImGui::SameLine();
+    filter_.Draw("Filter", -100.0f);
+    ImGui::Separator();
+    ImGui::BeginChild("LogOutput", ImVec2(0, 0), false, ImGuiWindowFlags_HorizontalScrollbar);
+    if (copy) ImGui::LogToClipboard();
+    if (filter_.IsActive())
+    {
+        const char* buf_begin = buffer_.begin();
+        const char* line = buf_begin;
+        for (int line_no = 0; line != NULL; line_no++)
+        {
+            const char* line_end = (line_no < lineOffsets_.Size) ? buf_begin + lineOffsets_[line_no] : NULL;
+            if (filter_.PassFilter(line, line_end))
+            {
+                ImGui::TextUnformatted(line, line_end);
+            }
+            line = line_end && line_end[1] ? line_end + 1 : NULL;
+        }
+    }
+    else
+    {
+        ImGui::TextUnformatted(buffer_.begin());
+    }
 
+    if (scrollToBottom_)
+    {
+        ImGui::SetScrollHere(1.0f);
+    }
+    scrollToBottom_ = false;
+    ImGui::EndChild();
+}
+
+void OutputPanel::clear()
+{
+    buffer_.clear(); lineOffsets_.clear();
+}
+
+void OutputPanel::log(const char* fmt, ...) IM_FMTARGS(2)
+{
+    int old_size = buffer_.size();
+    va_list args;
+    va_start(args, fmt);
+    buffer_.appendv(fmt, args);
+    va_end(args);
+    for (int new_size = buffer_.size(); old_size < new_size; old_size++)
+        if (buffer_[old_size] == '\n')
+        {
+            lineOffsets_.push_back(old_size);
+        }
+    scrollToBottom_ = true;
 }
--- Source/Editor/OutputPanel.h
+++ Source/Editor/OutputPanel.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <imgui.h>
+
 #include "Editor/EditorPanel.h"
 
 class OutputPanel : public EditorPanel
@@ -9,4 +11,13 @@ public:
     virtual std::string name() const { return "Output Panel"; }
     virtual void draw();
 
+    void log(const char* fmt, ...);
+
+private:
+    ImGuiTextBuffer     buffer_;
+    ImGuiTextFilter     filter_;
+    ImVector<int>       lineOffsets_;
+    bool                scrollToBottom_;
+
+    void clear();
 };
