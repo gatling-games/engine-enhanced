0ddd8c7c2861696122a6beb11eb015276f0a4fb1
George Loines <loinesg@gmail.com>
2018-04-21 14:27:14 +0100

    Physics - Implement wip SphereCollider::checkForCollision

--- Source/Physics/SphereCollider.cpp
+++ Source/Physics/SphereCollider.cpp
@@ -1,5 +1,8 @@
 #include "SphereCollider.h"
+
 #include "imgui.h"
+
+#include "Scene/Transform.h"
 
 SphereCollider::SphereCollider(GameObject* gameObject)
     : Collider(gameObject),
@@ -33,5 +36,31 @@ void SphereCollider::setOffset(const Vector3& offset)
 
 bool SphereCollider::checkForCollision(Point3 point, float radius, ColliderHit& hit) const
 {
+    // Get the local space centre of the collider in world space
+    const Point3 localSpaceCentre(offset_);
+    const Point3 worldSpaceCentre = gameObject()->transform()->localToWorld() * localSpaceCentre;
+
+    // Find the vector between the two centres
+    const Vector3 colliderToPoint = point - worldSpaceCentre;
+
+    // Scale the radius by the transfrm scale
+    const float radiusScale = gameObject()->transform()->scaleWorld();
+
+    // A hit occurs if the 2 radii are greater than the distance between the colliders
+    if(colliderToPoint.magnitude() < (radius + radius_))
+    {
+        std::cout << "Point at " << point << std::endl;
+        std::cout << "Point radius " << radius << std::endl;
+        std::cout << "Collider at " << worldSpaceCentre << std::endl;
+        std::cout << "Collider radius " << radius_ << std::endl;
+        printf("Collision with %s \n", gameObject()->name().c_str());
+
+        hit.position = worldSpaceCentre + colliderToPoint.normalized() * radius_;
+        hit.normal = colliderToPoint.normalized();
+        return true;
+    }
+    else
+    {
         return false;
+    }
 }
