4978e3b5f0135ba027b71ec30b7310933b40365c
Joshua Crinall <crinalljoshua@gmail.com>
2018-04-26 18:03:38 +0100

    Turrets - Added TurretGun to project/filters and implemented class files

---
+++ Source/Scene/TurretGun.cpp
@@ -0,0 +1,46 @@
+#include "TurretGun.h"
+
+#include "Scene/Transform.h"
+#include "Math/Quaternion.h"
+#include "Serialization/Prefab.h"
+
+#include "imgui.h"
+#include "Utils/ImGuiExtensions.h"
+
+TurretGun::TurretGun(GameObject* gameObject)
+    : Component(gameObject),
+    transform_(gameObject->createComponent<Transform>()),
+    timeSinceShot_(0.0f),
+    refireTime_(2.5f)
+{
+    transform_->setRotationLocal(Quaternion::identity());
+}
+
+void TurretGun::drawProperties()
+{
+    ImGui::ResourceSelect<Prefab>("Prefab", "Select Prefab", prefab_);
+    ImGui::DragFloat("Refire time", &refireTime_, 0.1f);
+}
+
+void TurretGun::update(float deltaTime)
+{
+    // Add delta time to elapsed time since shot
+    timeSinceShot_ += deltaTime;
+    
+    // If elapsed time exceeds refire time, fire projectile
+    if (timeSinceShot_ >= refireTime_)
+    {
+        spawnPrefab();
+        timeSinceShot_ = 0.0f;
+    }
+}
+
+void TurretGun::spawnPrefab()
+{
+    if (prefab_ != nullptr)
+    {
+        // Create new gameObject using prefab and set parent transform
+        GameObject* projectile = new GameObject("Projectile", prefab_);
+        projectile->transform()->setParentTransform(transform_);
+    }
+}
