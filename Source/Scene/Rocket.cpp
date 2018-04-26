74a6b6f21ccb31a6cfd7a566ff346f7f28484058
Joshua Crinall <crinalljoshua@gmail.com>
2018-04-26 18:06:19 +0100

    Turrets - Moved Rigidbody initialisation to rocket constructor and removed update override

--- Source/Scene/Rocket.cpp
+++ Source/Scene/Rocket.cpp
@@ -10,9 +10,18 @@
 Rocket::Rocket(GameObject* gameObject)
     : Component(gameObject),
     transform_(gameObject->createComponent<Transform>()),
-    speed_(200.0f)
+    speed_(100.0f)
 {
+    // Initialise rotation
     transform_->setRotationLocal(Quaternion::identity());
+
+    // Create rigidbody for collisions
+    gameObject->createComponent<Rigidbody>();
+    Rigidbody* collider = gameObject->findComponent<Rigidbody>();
+
+    // Calculate velocity and send to rigidbody component
+    Vector3 velocity = transform_->forwards() * speed_;
+    collider->setVelocity(velocity);
 }
 
 void Rocket::drawProperties()
@@ -20,22 +29,8 @@ void Rocket::drawProperties()
     ImGui::DragFloat("Rocket speed", &speed_, 0.1f);
 }
 
-void Rocket::update(float deltaTime)
-{
-    if (initialised_ == false)
-    {
-        Rigidbody* collider;
-        if (collider = gameObject()->findComponent<Rigidbody>())
-        {
-            Vector3 velocity = transform_->forwards() * speed_;
-            collider->setVelocity(velocity);
-
-            initialised_ = true;
-        }
-    }
-}
-
 void Rocket::handleCollision(Collider* collider)
 {
+    // Delete gameObject on collision
     delete gameObject();
 }
