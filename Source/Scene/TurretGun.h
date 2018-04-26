03653ba3b5f6ed77b2a812cd0d981f506138a492
Joshua Crinall <crinalljoshua@gmail.com>
2018-04-26 18:03:38 +0100

    Turrets - Added TurretGun to project/filters and implemented class files

---
+++ Source/Scene/TurretGun.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Scene/Component.h"
+
+class Prefab;
+
+class TurretGun : public Component
+{
+public:
+    TurretGun(GameObject* gameObject);
+
+    void drawProperties() override;
+    void update(float deltaTime) override;
+
+    void spawnPrefab();
+
+private:
+    Transform* transform_;
+    Prefab* prefab_ = nullptr;
+
+    float timeSinceShot_;
+    float refireTime_;
+};
